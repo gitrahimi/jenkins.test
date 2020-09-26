63b50878f9d4a3bb15b60e8b97ccde8fb0611fa3
Rahimi <58125547+gitrahimi@users.noreply.github.com>
2020-09-25 21:05:07 -0500

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "This is a very simple script for testing Jenkins   - v2"  
+echo "This is a very simple script for testing Jenkins   - v5"  
 
-echo " Thi is Awesome for testing "
+echo " Thi is Awesome for testing-sept25 "
